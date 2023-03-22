2b7cf95b2a82dc4c0f7ce3a73ee7f13b71b7e283
facundo <facuodone1@gmail.com>
2023-03-22 17:39:48 -0300

    Ejercicio 2

--- ejercicio_1/src/test/java/ar/edu/unlp/info/oo2/ejercicio_1/TwitterTest.java
+++ ejercicio_1/src/test/java/ar/edu/unlp/info/oo2/ejercicio_1/TwitterTest.java
@@ -2,6 +2,7 @@ package ar.edu.unlp.info.oo2.ejercicio_1;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
